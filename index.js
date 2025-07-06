2e4181cc883b9f9e0a95d4973e5bccf071463124
TarzanGarg <tarzan6239@gmail.com>
2025-07-05 23:37:04 -0400

    patch added

--- index.js
+++ index.js
@@ -37,6 +37,45 @@ app.get('/todos/:id', (req, res) => {
   });
 })
 
+
+
+app.patch('/todos/:id', (req, res) =>{
+  const id = req.params.id;
+  let query = 'UPDATE todolist SET ';
+  let params = []
+  if (!!req.body.task) {
+    query += 'task = ? ';
+    params.push(req.body.task);
+  }
+
+  if (req.body?.isCompleted === true || req.body?.isCompleted === false) {
+    if (params.length > 0) {
+      query += ',';
+    }
+
+    query += 'iscompleted = ? ';
+    params.push(req.body.isCompleted);
+  }
+
+  if (params.length > 0) {
+    query += 'WHERE id = ?'
+    params.push(id);
+    console.log(query);
+    db.run(query, params, (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error updating todo');
+      }
+      else {
+        res.sendStatus(200);
+      }
+    });
+  }
+  else {
+    res.sendStatus(204);
+  }
+})
+
 app.post('/todos', (req, res) => {
     const { task, iscompleted} = req.body;
     const date = new Date(Date.now()).toISOString();
@@ -47,6 +86,19 @@ app.post('/todos', (req, res) => {
       res.status(500).send('Error inserting todo');
     } else {
       res.status(201).json({id : this.lastID, task, iscompleted, createddate:date });
+    }
+  })
+})
+
+app.delete('/todos/:id',(req,res) => {
+  const query = 'DELETE FROM todolist WHERE id=?';
+  const id = req.params.id;
+  db.run( query, [id], (err) => {
+    if(err){
+      res.sendStatus(500);
+    }
+    else {
+      res.sendStatus(204)
     }
   })
 })
